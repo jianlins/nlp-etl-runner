b3de1fbb7f7500cae9f917c1c87a02a621ba72f0
jianlinshi <jianlin.shi@va.gov>
2022-09-30 09:41:18 -0600

    1.2

--- src/main/java/gov/va/vinci/etl/SingleScriptRunner.java
+++ src/main/java/gov/va/vinci/etl/SingleScriptRunner.java
@@ -7,10 +7,7 @@ import org.apache.commons.io.FilenameUtils;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,19 +26,38 @@ public class SingleScriptRunner {
     protected String scriptLocation = "";
     protected boolean wait = true;
     protected String successStr = "";
-    protected HashMap<String, Map<String,String>> failureDict = new HashMap<>();
+    protected HashMap<String, Map<String, String>> failureDict = new HashMap<>();
     protected File logDir = null;
     protected String args = "";
     protected SimpleDateFormat date_format = new SimpleDateFormat("MM/dd/yyy HH:mm:ss");
-    protected int repeat=1;
-    protected int repeatInterval=5;
+    protected int repeat = 1;
+    protected int repeatInterval = 5;
+
+    public SingleScriptRunner(Iterable<String> args) {
+        LinkedHashMap<String, String> config = new LinkedHashMap<>();
+        ArrayList<String> argList = new ArrayList<>();
+        for (String arg : args)
+            argList.add(arg);
+        if (argList.size() == 0) {
+            throw new IllegalArgumentException("args need to have at least one item to specify which command to run");
+        }
+        config.put("location", argList.get(0));
+        if (argList.size() > 1) {
+            config.put("args", String.join(" ", argList.subList(1, argList.size())));
+        }
+        init(config);
+    }
 
     public SingleScriptRunner(Map scriptConfig) {
+        init(scriptConfig);
+    }
+
+    public void init(Map scriptConfig) {
         if (scriptConfig.containsKey("location")) {
             this.scriptLocation = (String) scriptConfig.get("location");
-            this.scriptLocation=new File(this.scriptLocation).getAbsolutePath();
-            if (!new File(this.scriptLocation).exists()){
-                LOGGER.warning("The script file doesn't exist. This execution will be skipped. Please check: "+this.scriptLocation);
+            this.scriptLocation = new File(this.scriptLocation).getAbsolutePath();
+            if (!new File(this.scriptLocation).exists()) {
+                LOGGER.warning("The script file doesn't exist. This execution will be skipped. Please check: " + this.scriptLocation);
             }
         } else {
             LOGGER.warning("The location of script has not been set. This execution will be skipped.");
@@ -54,14 +70,14 @@ public class SingleScriptRunner {
             LOGGER.info("script name has not been set, infer a name from script file name as: " + this.scriptName);
         }
         if (scriptConfig.containsKey("wait")) {
-            Object value= scriptConfig.get("wait");
-            this.wait=value.toString().toLowerCase().startsWith("t");
+            Object value = scriptConfig.get("wait");
+            this.wait = value.toString().toLowerCase().startsWith("t");
         }
-        if (scriptConfig.containsKey("repeat") && scriptConfig.get("repeat").toString().length()>0){
-            this.repeat=(int)Double.parseDouble(scriptConfig.get("repeat").toString());
+        if (scriptConfig.containsKey("repeat") && scriptConfig.get("repeat").toString().length() > 0) {
+            this.repeat = (int) Double.parseDouble(scriptConfig.get("repeat").toString());
         }
-        if (scriptConfig.containsKey("repeat_interval") && scriptConfig.get("repeat_interval").toString().length()>0){
-            this.repeatInterval=Integer.parseInt(scriptConfig.get("repeat_interval").toString());
+        if (scriptConfig.containsKey("repeat_interval") && scriptConfig.get("repeat_interval").toString().length() > 0) {
+            this.repeatInterval = Integer.parseInt(scriptConfig.get("repeat_interval").toString());
         }
         if (scriptConfig.containsKey("success")) {
             this.successStr = (String) scriptConfig.get("success");
@@ -79,12 +95,12 @@ public class SingleScriptRunner {
         if (scriptConfig.containsKey("args")) {
             args = (String) scriptConfig.get("args");
         } else {
-            LOGGER.logp(Level.INFO, "", "","The success indication string has not been set in the configuration file.");
+            LOGGER.logp(Level.INFO, "", "", "The success indication string has not been set in the configuration file.");
         }
         if (scriptConfig.containsKey("logdir")) {
             this.logDir = new File((String) scriptConfig.get("logdir"));
             if (!logDir.exists()) {
-                LOGGER.logp(Level.INFO, "", "","log directory doesn't exist, try to create one at: " + logDir.getAbsolutePath());
+                LOGGER.logp(Level.INFO, "", "", "log directory doesn't exist, try to create one at: " + logDir.getAbsolutePath());
                 try {
                     FileUtils.forceMkdir(logDir);
                 } catch (IOException e) {
@@ -92,33 +108,33 @@ public class SingleScriptRunner {
                 }
             }
         } else {
-            LOGGER.logp(Level.INFO, "", "","logdir hasn't been set, will skip logging to local file.");
+            LOGGER.logp(Level.INFO, "", "", "logdir hasn't been set, will skip logging to local file.");
         }
     }
 
     public int[] executeTimes(int times) throws IOException, InterruptedException {
-        String[]argsArray=args.split(" +");
-        String[]combinedArgs=new String[argsArray.length+1];
-        combinedArgs[0]=this.scriptLocation;
+        String[] argsArray = args.split(" +");
+        String[] combinedArgs = new String[argsArray.length + 1];
+        combinedArgs[0] = this.scriptLocation;
         System.arraycopy(argsArray, 0, combinedArgs, 1, argsArray.length);
-        final int[] status=new int[times];
-        ArrayList<CmdRunnable>runners=new ArrayList<>();
-        for (int i=0;i<times;i++) {
+        final int[] status = new int[times];
+        ArrayList<CmdRunnable> runners = new ArrayList<>();
+        for (int i = 0; i < times; i++) {
             ProcessBuilder pb = new ProcessBuilder(combinedArgs);
             Process process = pb.start();
-            CmdRunnable runner=new CmdRunnable(i, scriptName, process, LOGGER,  successStr, failureDict);
+            CmdRunnable runner = new CmdRunnable(i, scriptName, process, LOGGER, successStr, failureDict);
             runners.add(runner);
             new Thread(runner).start();
         }
-        if (wait){
-            for (int id=0;id<times;id++){
-                if(runners.get(id).getProcess().isAlive()){
+        if (wait) {
+            for (int id = 0; id < times; id++) {
+                if (runners.get(id).getProcess().isAlive()) {
                     TimeUnit.SECONDS.sleep(5);
-                    LOGGER.logp(Level.INFO, "", "","Java Process ("+scriptName+":"+id+") current status:" + runners.get(id).status);
+                    LOGGER.logp(Level.INFO, "", "", "Java Process (" + scriptName + ":" + id + ") current status:" + runners.get(id).status);
                     id--;
-                }else{
-                    LOGGER.logp(Level.INFO, "", "","Java Process ("+scriptName+":"+id+") result:" + runners.get(id).status);
-                    status[id]=runners.get(id).status;
+                } else {
+                    LOGGER.logp(Level.INFO, "", "", "Java Process (" + scriptName + ":" + id + ") result:" + runners.get(id).status);
+                    status[id] = runners.get(id).status;
                 }
             }
         }
@@ -198,13 +214,13 @@ public class SingleScriptRunner {
 //        return status[0];
 //    }
 
-    final static boolean checkFailure(String line, Map<String, String> stringStringHashMap, HashMap<String, Pattern>  failureRegex) {
-        if (stringStringHashMap.containsKey("text")){
+    final static boolean checkFailure(String line, Map<String, String> stringStringHashMap, HashMap<String, Pattern> failureRegex) {
+        if (stringStringHashMap.containsKey("text")) {
             return line.contains(stringStringHashMap.get("text"));
-        }else if (stringStringHashMap.containsKey("regex")){
-            String regex=stringStringHashMap.get("regex");
-            if (!failureRegex.containsKey(regex)){
-                Pattern p=Pattern.compile(regex);
+        } else if (stringStringHashMap.containsKey("regex")) {
+            String regex = stringStringHashMap.get("regex");
+            if (!failureRegex.containsKey(regex)) {
+                Pattern p = Pattern.compile(regex);
                 failureRegex.put(regex, p);
             }
             return failureRegex.get(regex).matcher(line).find();
